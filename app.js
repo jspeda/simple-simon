b0465ee81679fde28ed8838fbb579ab2b04ccaf3
Joseph Spedale <jspedale@gmail.com>
2016-12-29 12:01:51 -0500

    Added skeleton JS file w Simon object and functions

---
+++ app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+var Simon = {
+  init: function() {
+    this.colors = ["green", "red", "yellow", "blue"];
+  }
+};
+
+Game = Object.create(Simon);
+
+Game.start = function() {
+
+};
+
+Game.strict = function() {
+
+};
+
+Game.restart = function() {
+
+};
+
+
+
+});
