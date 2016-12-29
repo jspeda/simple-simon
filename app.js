491a9e817d0b8bd3696a5c8c76ac7027e3b3bf91
Joe Spedale <jspedale@gmail.com>
2016-12-29 16:26:06 -0500

    Upon game start, logs random colors until 25 and stores history in an array

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ $(document).ready(function() {
 var Simon = {
   init: function() {
     this.colors = ["green", "red", "yellow", "blue"];
+    this.count = 0;
+    this.previous = [];
   }
 };
 
-Game = Object.create(Simon);
+var Game = Object.create(Simon);
 
 Game.start = function() {
+  var simonSays = setInterval(function() {
+    var selection = this.colors[Math.floor(Math.random() * (4 - 0) + 0)];
+    console.log(selection);
+    this.previous.push(selection);
 
+    //$("." + this.colors[selection]).css('background', 'rgba(0,0,0,0)');
+    this.count++;
+    if (this.count === 25) {
+      clearInterval(simonSays);
+      console.log(this.previous);
+    }
+  }.bind(this), 1000);
 };
 
 Game.strict = function() {
@@ -20,6 +33,14 @@ Game.restart = function() {
 
 };
 
+Game.init();
+Game.start();
+
+$('.green, .red, .yellow, .blue').mousedown(function() {
+  console.log("clicked " + $(this).attr('class'));
+  // $(this).css('background', 'rgba(0,0,0,0.05)');
+  //background: rgba(0,0,0,0.1);
+});
 
 
 });
